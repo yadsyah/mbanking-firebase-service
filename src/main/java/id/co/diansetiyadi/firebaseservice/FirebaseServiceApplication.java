7241942b58ea8b248ba48ddad4b629298f7704b7
yadsyah <diyansetiyadi@gmail.com>
2022-12-29 17:44:40 +0700

    init commit

---
+++ src/main/java/id/co/diansetiyadi/firebaseservice/FirebaseServiceApplication.java
@@ -0,0 +1,13 @@
+package id.co.diansetiyadi.firebaseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirebaseServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/diansetiyadi/firebaseservice/FirebaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.diansetiyadi.firebaseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebaseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
